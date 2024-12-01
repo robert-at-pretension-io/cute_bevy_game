8ed8a35d9fa03adcfcd8283571ffeb8476823edb
Robert Elliot Pahel-Short <rep@akc.org>
2024-12-01 14:50:17 -0500

    fixed?

---
+++ pkg/ball_drop_rust_bg.wasm.d.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const wasm_main: () => void;
+export const wgpu_render_bundle_set_pipeline: (a: number, b: bigint) => void;
+export const wgpu_render_bundle_set_bind_group: (a: number, b: number, c: bigint, d: number, e: number) => void;
+export const wgpu_render_bundle_set_vertex_buffer: (a: number, b: number, c: bigint, d: bigint, e: bigint) => void;
+export const wgpu_render_bundle_set_push_constants: (a: number, b: number, c: number, d: number, e: number) => void;
+export const wgpu_render_bundle_draw: (a: number, b: number, c: number, d: number, e: number) => void;
+export const wgpu_render_bundle_draw_indexed: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+export const wgpu_render_bundle_draw_indirect: (a: number, b: bigint, c: bigint) => void;
+export const wgpu_render_bundle_draw_indexed_indirect: (a: number, b: bigint, c: bigint) => void;
+export const wgpu_render_bundle_set_index_buffer: (a: number, b: bigint, c: number, d: bigint, e: bigint) => void;
+export const wgpu_render_bundle_pop_debug_group: (a: number) => void;
+export const wgpu_render_bundle_insert_debug_marker: (a: number, b: number) => void;
+export const wgpu_render_bundle_push_debug_group: (a: number, b: number) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_4: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0c2982553b67613e: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut__A_B___Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0b351159393bee00: (a: number, b: number, c: number, d: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdaacbe0609476dc7: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9302db5f9e8a4b2e: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h040d184f00a7eb90: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
